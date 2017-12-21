c68d18e7792018d229d47166372c2344726b799f
Aum <suwicha.sc@gmail.com>
2017-12-21 17:45:27 +0700

    workspace

---
+++ workspace/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'theme/header.php'; ?>
+
+<body>
+  <div class="header-main-wrap">navbar</div>
+  <div id="body-main-wrap" style="transform: none;">body-main
+    <div class="slide-main-warp">slide</div>
+    <div class="contain-main-warp">container</div>
+  </div>
+
+</body>
+
+</html>
---
+++ workspace/theme/header.php
@@ -0,0 +1,9 @@
+<head>
+  <title>Bootstrap</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="bootstrap/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="/css/style.css" />
+</head>
